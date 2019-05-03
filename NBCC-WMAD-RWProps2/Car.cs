5eb001467323ea1269732f29ec2c3207a4208109
owlgirl07 <jessica.alyssa.07@live.ca>
2019-05-03 10:37:08 -0300

    created car class

---
+++ NBCC-WMAD-RWProps2/Car.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBCC_WMAD_Console
+{
+    public class Car
+    {
+        //That's why you need to instantiate or whatever string variables before adding to it while int you don't have to
+        //Nor did I, but I would get run time errors for it all the time and now it finally makes sense why
+        //I'll try it later!
+        private int? speed;
+        private Nullable<decimal> price;
+        private int numberOfDoors;
+        private string carType;
+
+        public int? Year { get; set; }
+
+        public int? Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public Nullable<decimal> Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+
+        public int NumberOfDoors
+        {
+            get
+            {
+                return numberOfDoors;
+            }
+            set
+            {
+                if (value < 10)
+                {
+                    numberOfDoors = value;
+                }
+            }
+        }
+
+        public string CarType
+        {
+            get
+            {
+                return carType;
+            }
+            set
+            {
+                carType = value;
+            }
+        }
+    }
+}
--- NBCC-WMAD-RWProps2/Program.cs
+++ NBCC-WMAD-RWProps2/Program.cs
@@ -15,7 +15,8 @@ namespace NBCC_WMAD_Console
         private static void LoadMenu()
         {
             Menu.menuDictionary.Add(0, "Exit");
-            Menu.menuDictionary.Add(1, "About the Console");            
+            Menu.menuDictionary.Add(1, "About the Console");
+            Menu.menuDictionary.Add(2, "Car Builder");
         }
 
         static void Main(string[] args)
@@ -33,13 +34,56 @@ namespace NBCC_WMAD_Console
                         AboutThisApp();
                         break;
                     //Add more options in the menu here
+                    case 2:
+                        CarBuilder();
+                        break;
                 }
             }
 
             Out.P("Press any key to exit.");
             Console.ReadLine();
         }
-                
+
+        //I want to store the cars I create
+        static List<Car> myCars = new List<Car>();
+
+        private static void CarBuilder()
+        {
+            string userCarType = In.GetString("Provide a car type (ie Car, Truck, Van: \n");
+            int userNumberOfDoors = In.GetInt("Provide the number of doors: \n");
+            int userSpeed = In.GetInt("Provide car speed: \n");
+
+            Car car = new Car();
+
+            car.CarType = userCarType;
+            car.NumberOfDoors = userNumberOfDoors;
+            car.Speed = userSpeed;
+
+            Out.P("***********");
+            Out.P($"The car created is:\nType: {car.CarType}\nNumber of doors: {car.NumberOfDoors}\nSpeed: {car.Speed}\nPrice: {car.Price.GetValueOrDefault()}");
+            Out.P("***********\n");
+
+            myCars.Add(car);
+
+            Out.P($"I have a car count in my list of: {myCars.Count}");
+
+            /*After added to the list:
+             * Prompt the user: "Do you want to list all cars? Y/N"
+             * If yes: Loop throught the list of cars and print each car with all properties
+             * (right after the car count)
+             */
+
+            if(In.GetString("Do you want to list all cars?\n") == "Y")
+            {
+                //loop
+            }
+            else if(In.GetString("Do you want to list all cars?\n") == "N")
+            {
+                return;
+            }
+            
+        }
+
         #region [My Functionality]
 
         /*
